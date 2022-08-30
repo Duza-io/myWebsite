622b7938ae7efa988f5c570623b63acc32147db4
MARK DUZA <markandrewduza19@gmail.com>
2022-08-30 17:08:44 +0800

    updated button

--- src/components/About.js
+++ src/components/About.js
@@ -16,11 +16,11 @@ export default function About() {
                         Laborum, voluptas natus?
                     </p>
                     <div className="flex justify-center">
-                        <button href="#contact" className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-black dark:text-white focus:none focus:outline-none focus:outline-none dark:focus:outline-none">
+                        <a href="#contact" className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-black dark:text-white focus:none focus:outline-none focus:outline-none dark:focus:outline-none">
                             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-black rounded-md group-hover:bg-opacity-0">
                                 Contact Me!
                             </span>
-                        </button>
+                        </a>
                         {/*  <a
                             href="#projects"
                             className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
