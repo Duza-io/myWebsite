d6c0ef4faab97c28e2382186bed6c0bc7c9adb4c
MARK DUZA <markandrewduza19@gmail.com>
2022-09-03 23:24:56 +0800

    crazy

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ export default function About() {
                     </p>
                     <div className="flex justify-center">
                         <a href="#contact" className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-black dark:text-white focus:none focus:outline-none focus:outline-none dark:focus:outline-none">
-                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-black rounded-md group-hover:bg-opacity-0">
+                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-black rounded-md group-hover:bg-opacity-0">
                                 Contact Me!
                             </span>
                         </a>
